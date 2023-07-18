31eb3ae1dd7978d6748526b7eb28406a751ca552
sahdevmandal123 <122348254+sahdevmandal123@users.noreply.github.com>
2023-07-19 02:03:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" 
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
